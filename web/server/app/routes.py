bc3fd4c641b1166699cbff669298a36f2461fd04
Anatoly Rubanov <anatolijrubanov@gmail.com>
2019-11-10 20:54:44 +0300

    move db session to app context

--- web/server/app/routes.py
+++ web/server/app/routes.py
@@ -19,17 +19,23 @@ schema = validation.ImageSchema()
 app.config['UPLOAD_FOLDER'] = PHOTO_DIR
 
 
+def get_db():
+    if 'db' not in g:
+        g.db = Session()
+
+    return g.db
+
+
 @app.route('/photos', methods=['POST'])
 def post_photo():
     try:
-        print(request.data, request.form, request.args, request.values, request.files)
         js = schema.load(request.form)
         image_id = js['image_id']
         path = os.path.join(app.config['UPLOAD_FOLDER'], str(image_id))
         file = request.files['photo']
         if file:
             file.save(path)
-            session = g.session
+            session = get_db()
             item = Image(id=image_id, path=path, result=None)
             session.add(item)
             session.commit()
@@ -41,7 +47,7 @@ def post_photo():
 
 @app.route('/photos/<int:image_id>/', methods=['GET'])
 def get_result(image_id):
-    session = g.session
+    session = get_db()
     item = session.query(Image).get(image_id)
     if item is None:
         abort(404, "Unexciting image id")
@@ -50,14 +56,12 @@ def get_result(image_id):
     item.result = result
     session.add(item)
     session.commit()
-    return jsonify({'expression':result})
-
+    return jsonify({'expression': result})
 
 
 @app.before_request
 def before_request():
     g.start = time.time()
-    g.session = Session()
 
 
 @app.after_request
@@ -65,5 +69,12 @@ def after_request(response):
     resp_time = (time.time() - g.start) * 1000  # время ответа сервера в миллисекндах
     logger.info('%s %s %s %s %s %s', request.remote_addr, request.method, request.scheme, request.full_path,
                 response.status, resp_time)
-    g.session.close()
     return response
+
+
+@app.teardown_appcontext
+def teardown_db(args):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
